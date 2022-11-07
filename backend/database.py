65132e0717783740f4a3c30eec268e2cfa02c870
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2022-11-06 20:13:41 -0500

    database edit

--- backend/database.py
+++ backend/database.py
@@ -6,9 +6,9 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 
-SQLALCHEMY_DATABASE_URL =  f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+# SQLALCHEMY_DATABASE_URL =  f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine("postgres://todo_ahd0_user:djsd6AnKzsd6F3xvM6pWaEbfiWqOK91r@dpg-cdk2qrien0honmehmvo0-a/todo_ahd0")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
